dae26c477c3bd78245fd9e4ffe37813b19fd4bbf
David McCrea <git@dmccrea.me>
2025-04-04 16:34:34 +0300

    Add layout

--- lib/edenflowers_web/live/courses_live.ex
+++ lib/edenflowers_web/live/courses_live.ex
@@ -11,9 +11,11 @@ defmodule EdenflowersWeb.CoursesLive do
 
   def render(assigns) do
     ~H"""
-    <div class="container my-36">
-      <h1 class="font-serif text-4xl">{gettext("Courses")}</h1>
-    </div>
+    <Layouts.app flash={@flash}>
+      <div class="container my-36">
+        <h1 class="font-serif text-4xl">{gettext("Courses")}</h1>
+      </div>
+    </Layouts.app>
     """
   end
 end
