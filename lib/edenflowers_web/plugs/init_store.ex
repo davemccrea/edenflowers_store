736344ea30580575bafd4da3879fcaff7ad2773c
David McCrea <mail@dmccrea.me>
2025-05-05 22:15:56 +0300

    Check order_id is valid
    wip

--- lib/edenflowers_web/plugs/init_store.ex
+++ lib/edenflowers_web/plugs/init_store.ex
@@ -1,50 +1,20 @@
 defmodule EdenflowersWeb.Plugs.InitStore do
   import Plug.Conn
-
-  require Ash.Query
   require Logger
-  alias Edenflowers.Store.{Order, FulfillmentOption}
+  # Assuming Order.get_by_id/1 and Order.create/0 exist
+  alias Edenflowers.Store.Order
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    if get_session(conn, :order_id) do
-      # TODO: check order exists
+    order_id = get_session(conn, :order_id)
+
+    # Check order_id exists in session *and* order exists
+    if order_id && Order.get_by_id(order_id) do
       conn
     else
-      case create_new_order() do
-        {:ok, order} ->
-          put_session(conn, :order_id, order.id)
-
-        error ->
-          raise "Failed to initialize store: #{inspect(error)}"
-      end
+      order = Order.create!()
+      put_session(conn, :order_id, order.id)
     end
-  end
-
-  defp create_new_order do
-    with {:ok, fulfillment_option} <- get_fulfillment_option(),
-         {:ok, order} <- create_order(fulfillment_option.id) do
-      {:ok, order}
-    else
-      error -> error
-    end
-  end
-
-  defp get_fulfillment_option do
-    FulfillmentOption
-    |> Ash.Query.filter(fulfillment_method == :pickup)
-    |> Ash.read_one()
-    |> case do
-      {:ok, nil} -> {:error, :no_fulfillment_option}
-      {:ok, option} -> {:ok, option}
-      error -> error
-    end
-  end
-
-  defp create_order(fulfillment_option_id) do
-    Order
-    |> Ash.Changeset.for_create(:create, %{fulfillment_option_id: fulfillment_option_id})
-    |> Ash.create()
   end
 end
