b31f22426f40e9169407831f54596b2e1ce9a039
David McCrea <mail@dmccrea.me>
2025-05-10 17:56:38 +0300

    Stripe webhook handler should update payment status

--- lib/edenflowers_web/stripe_handler.ex
+++ lib/edenflowers_web/stripe_handler.ex
@@ -1,13 +1,26 @@
 defmodule EdenflowersWeb.StripeHandler do
   @behaviour Stripe.WebhookHandler
+
   require Logger
+
+  alias Edenflowers.Store.Order
 
   @impl true
   def handle_event(%Stripe.Event{type: "payment_intent.succeeded"} = event) do
-    dbg(event)
+    with %{data: %{object: %{metadata: %{"order_id" => order_id}}}} <- event,
+         {:ok, order} <- Order.payment_received(order_id) do
+      Logger.info("Payment received for order #{order.id}")
+    else
+      error ->
+        Logger.error("Error handling payment_intent.succeeded event: #{inspect(error)}")
+    end
+
     :ok
   end
 
   @impl true
-  def handle_event(_event), do: :ok
+  def handle_event(%Stripe.Event{type: type} = _event) do
+    Logger.warning("Unhandled Stripe event of type #{type}")
+    :ok
+  end
 end
