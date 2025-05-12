fa6205d4ed7341388d51fd025b351c0d490c5c1a
David McCrea <mail@dmccrea.me>
2025-05-12 11:51:15 +0300

    Add magic link auth strategy with migrations

--- lib/edenflowers/accounts/user.ex
+++ lib/edenflowers/accounts/user.ex
@@ -20,6 +20,16 @@ defmodule Edenflowers.Accounts.User do
       store_all_tokens? true
       require_token_presence_for_authentication? true
     end
+
+    strategies do
+      magic_link do
+        identity_field :email
+        registration_enabled? true
+        require_interaction? true
+
+        sender Edenflowers.Accounts.User.Senders.SendMagicLinkEmail
+      end
+    end
   end
 
   postgres do
@@ -36,6 +46,45 @@ defmodule Edenflowers.Accounts.User do
       get? true
       prepare AshAuthentication.Preparations.FilterBySubject
     end
+
+    read :get_by_email do
+      description "Looks up a user by their email"
+      get? true
+
+      argument :email, :ci_string do
+        allow_nil? false
+      end
+
+      filter expr(email == ^arg(:email))
+    end
+
+    create :sign_in_with_magic_link do
+      description "Sign in or register a user with magic link."
+
+      argument :token, :string do
+        description "The token from the magic link that was sent to the user"
+        allow_nil? false
+      end
+
+      upsert? true
+      upsert_identity :unique_email
+      upsert_fields [:email]
+
+      # Uses the information from the token to create or sign in the user
+      change AshAuthentication.Strategy.MagicLink.SignInChange
+
+      metadata :token, :string do
+        allow_nil? false
+      end
+    end
+
+    action :request_magic_link do
+      argument :email, :ci_string do
+        allow_nil? false
+      end
+
+      run AshAuthentication.Strategy.MagicLink.Request
+    end
   end
 
   policies do
@@ -50,5 +99,14 @@ defmodule Edenflowers.Accounts.User do
 
   attributes do
     uuid_primary_key :id
+
+    attribute :email, :ci_string do
+      allow_nil? false
+      public? true
+    end
+  end
+
+  identities do
+    identity :unique_email, [:email]
   end
 end
