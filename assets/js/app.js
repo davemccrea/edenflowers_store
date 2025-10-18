e32e7fe1fd681282a1004d4ffde12ab5680d4371
David McCrea <mail@dmccrea.me>
2025-10-18 13:37:33 +0300

    Fix typo

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,7 +32,7 @@ const csrfToken = document
 const liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
   params: { _csrf_token: csrfToken },
-  hooks: { Hooks, ...colocatedHooks },
+  hooks: { ...Hooks, ...colocatedHooks },
 });
 
 // Show progress bar on live navigation and form submits
