59308ec485f7a527594e3051ab7a5644d72c2dfd
David McCrea <mail@dmccrea.me>
2025-10-22 15:18:14 +0300

    Rewrite regen script for quicker development

--- regen.sh
+++ regen.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-# Get count of untracked migrations
-N_MIGRATIONS=$(git ls-files --others priv/repo/migrations | wc -l)
+mix ecto.drop
 
-# Rollback untracked migrations
-mix ash_postgres.rollback -n $N_MIGRATIONS
+find priv/repo/migrations -type f ! -name ".formatter.exs" -delete
+rm -rf priv/resource_snapshots
 
-# Delete untracked migrations and snapshots
-git ls-files --others priv/repo/migrations | xargs rm
-git ls-files --others priv/resource_snapshots | xargs rm
+mix ash_postgres.generate_migrations initial
+mix oban.install
 
-# Regenerate migrations
-mix ash.codegen --name $1
-
-# Run migrations if flag
-if echo $* | grep -e "-m" -q
-then
-  mix ash.migrate
-fi
+mix ash.setup
+mix run priv/repo/seeds.exs
