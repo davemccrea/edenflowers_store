69a54c8380cf58fe1f860869bc74ca309620a57a
David McCrea <mail@dmccrea.me>
2026-04-18 01:55:32 +0300

    Simplify the maintenance module

--- lib/edenflowers_web/plugs/maintenance.ex
+++ lib/edenflowers_web/plugs/maintenance.ex
@@ -1,46 +1,39 @@
 defmodule EdenflowersWeb.Plugs.Maintenance do
+  @moduledoc """
+  Redirects traffic to the maintenance page when maintenance mode is on.
+
+  Bypass with `?preview=<MAINTENANCE_BYPASS_SECRET>`; a session cookie keeps
+  subsequent requests flowing through.
+  """
+
   import Plug.Conn
+
+  @maintenance_path "/maternity"
+  @session_key :maintenance_bypass
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    if maintenance_mode?() do
-      case bypass_action(conn) do
-        :block ->
-          conn
-          |> Phoenix.Controller.redirect(to: "/maternity")
-          |> halt()
-
-        :allow ->
-          conn
-
-        {:set_session_and_allow, secret} ->
-          put_session(conn, :maintenance_bypass, secret)
-      end
-    else
-      conn
-    end
-  end
-
-  defp maintenance_mode? do
-    Application.get_env(:edenflowers, :maintenance_mode, false)
-  end
-
-  defp bypass_action(conn) do
-    secret = Application.get_env(:edenflowers, :maintenance_bypass_secret)
-
     cond do
-      conn.request_path == "/maternity" ->
-        :allow
+      not maintenance_mode?() ->
+        conn
 
-      secret != nil and conn.params["preview"] == secret ->
-        {:set_session_and_allow, secret}
+      conn.request_path == @maintenance_path ->
+        conn
 
-      secret != nil and get_session(conn, :maintenance_bypass) == secret ->
-        :allow
+      conn.params["preview"] == bypass_secret() ->
+        put_session(conn, @session_key, bypass_secret())
+
+      get_session(conn, @session_key) == bypass_secret() ->
+        conn
 
       true ->
-        :block
+        conn
+        |> Phoenix.Controller.redirect(to: @maintenance_path)
+        |> halt()
     end
   end
+
+  defp maintenance_mode?, do: Application.get_env(:edenflowers, :maintenance_mode, false)
+  defp bypass_secret, do: Application.get_env(:edenflowers, :maintenance_bypass_secret)
 end
