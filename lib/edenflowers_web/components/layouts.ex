7cede7a29461162245aedd64a1547692771fcb65
David McCrea <mail@dmccrea.me>
2026-05-08 17:34:21 +0300

    Format layouts.ex

--- lib/edenflowers_web/components/layouts.ex
+++ lib/edenflowers_web/components/layouts.ex
@@ -256,7 +256,7 @@ defmodule EdenflowersWeb.Layouts do
 
                 <%= if not is_nil(@order.total_items_in_cart) && @order.total_items_in_cart > 0 do %>
                   <span class="absolute top-0 right-0 lg:hidden">
-                    <div class="bg-primary text-primary-content border-base-100 h-5 w-5 text-[10px] inline-flex items-center justify-center rounded-full border-2 font-semibold leading-none">
+                    <div class="bg-primary text-primary-content border-base-100 text-[10px] inline-flex h-5 w-5 items-center justify-center rounded-full border-2 font-semibold leading-none">
                       {@order.total_items_in_cart}
                     </div>
                   </span>
