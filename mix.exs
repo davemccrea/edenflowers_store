5f263c7a0e29704c3193d8f7de2a0bad702b35de
David McCrea <mail@dmccrea.me>
2026-06-04 23:40:38 +0300

    Bump version to v0.4.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Edenflowers.MixProject do
   def project do
     [
       app: :edenflowers,
-      version: "0.4.1",
+      version: "0.4.2",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
